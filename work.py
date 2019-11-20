adc05a1b9013cda35bf2b055eba668166a689d03
ElijahCoding <cuiyeqing960904@gmail.com>
2019-11-20 21:53:39 +0300

    create new jobs file

--- work.py
+++ work.py
@@ -26,3 +26,39 @@ if req.status_code == 200:
         pages = pagination.find_all('li', attrs={'class': False})
         for page in pages:
             urls.append(domain + page.a['href'])
+
+
+for url in urls:
+    time.sleep(2)
+    req = session.get(url, headers=headers)
+    if req.status_code == 200:
+        bsObj = BS(req.content, "html.parser")
+        div_list = bsObj.find_all('div', attrs={'class': 'job-link'})
+        for div in div_list:
+            title = div.find('h2')
+            href = title.a['href']
+            short = div.p.text
+            company = "No name"
+            logo = div.find('img')
+            if logo:
+                company = logo['alt']
+            jobs.append({
+                'href': domain + href,
+                'title': title.text,
+                'descript': short,
+                'company': company
+            })
+
+
+template = '<!doctype html><html lang="en"><head><meta charset="utf-8"></head><body>'
+end = '</body></html>'
+content = '<h2> Work.ua</h2>'
+
+for job in jobs:
+    content += '<a href="{href}" target="_blank">{title}</a><br/><p>{descript}</p><p>{company}</p><br/>'.format(**job)
+    content += '<hr/><br/><br/>'
+
+data = template + content + end
+handle = codecs.open('jobs.html', "w", 'utf-8')
+handle.write(str(data))
+handle.close()
